0fd3d80aa1ac43d2fa7666d5cf66db13c92437d3
Connor Koch <connorkoch@mymail.mines.edu>
2018-11-26 11:47:43 -0700

    Update login

--- loginPage.php
+++ loginPage.php
@@ -1,23 +1,45 @@
- <form action="action_page.php">
-  <div class="imgcontainer">
-    <img src="img.png" alt="image" class="avatar">
-  </div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Weight</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Weight">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginCss.css">
 
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="username" required>
+    <script>
+    function validateForm() {
+        var x = document.forms["myForm"]["fname"].value;
+        if (x == "") {
+            alert("Name must be filled out");
+            return false;
+        }
+    }
+    </script>
+</head>
+<body>
+    <form name="loginForm" action="index.php" method="post">
+        <img src="loginIcon.jpg" alt="image" class="logo">
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" required>
+        <div class="container">
+            <label class="labe" for="uname"><b>Username</b></label>
+            <input class="textin" type="text" placeholder="Enter Username" name="username" required>
 
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
+            <label class="labe" for="psw"><b>Password</b></label>
+            <input class="textin" type="password" placeholder="Enter Password" name="password" required>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
+            <div class="submitbar">
+                <button class="login" type="submit">Login</button>
+                <label class="check">
+                    <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+            </div>
+            <div class="cancelbar" style="background-color:#f1f1f1">
+                <a href="#">Create Account</a>
+                ---
+                <a href="#">Forgot Password?</a>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
